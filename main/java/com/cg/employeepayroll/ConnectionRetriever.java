fa7486cf5ea67b1b75877be08807a750ada8c720
Abhinav9p16 <thakurabhinav56@gmail.com>
2020-11-22 22:36:11 +0530

    Added: Code to establish connection

---
+++ main/java/com/cg/employeepayroll/ConnectionRetriever.java
@@ -0,0 +1,34 @@
+package com.cg.employeepayroll;
+
+import java.sql.*;
+import java.util.*;
+
+public class ConnectionRetriever {
+    public static synchronized Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String userName = "root";
+        String password = "abhi9p16";
+        Connection connection;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        listDrivers();
+        try {
+            connection = DriverManager.getConnection(url, userName, password);
+            return connection;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return null;
+    }
+
+    private static void listDrivers() {
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+        }
+    }
+}
+
